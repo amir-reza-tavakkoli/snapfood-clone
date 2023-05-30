4f093623aad9bea055738c47a8bbbffcc62e2a6a
Amir <tavakkoli13@gmail.com>
2023-05-30 12:42:00 +0330

    Chore/remixify (#29)
    * add remix and project deps
    
    * configure index.css
    
    * add prisma migrations to be ignored

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import "./base.css"
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <h1>hhh</h1>
-  </React.StrictMode>,
-)
