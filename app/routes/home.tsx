bd40866ec992d9eec7add4ae6a930832ecaaab79
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2023-06-14 20:39:59 +0330

    incorporate headers

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,40 +1,92 @@
-import { Address, User } from "@prisma/client"
-import { Link, Outlet, useLoaderData, useNavigate } from "@remix-run/react"
-import { LoaderArgs } from "@remix-run/server-runtime"
-import { createContext, useContext, useEffect } from "react"
-import { getNearestAddress } from "~/utils/address.query.server"
-import { calculateOrder } from "~/utils/order.query.server"
+import { Address, StoreKind } from "@prisma/client"
+import { Outlet, useLoaderData} from "@remix-run/react"
+import { LinksFunction, LoaderArgs } from "@remix-run/server-runtime"
+import { useEffect, useState } from "react"
+import { Header } from "~/components/header"
+import { CategoryNav } from "~/components/nav"
+
+import {
+  getUserAddresses,
+} from "~/utils/address.query.server"
 
 import { requirePhoneNumber } from "~/utils/session.server"
 import { getUserByPhone } from "~/utils/user.query.server"
+import { getStoresKind } from "~/utils/store.query.server"
+
+import headerCss from "./../components/header.css"
+import buttonCss from "./../components/button.css"
+import iconCss from "./../components/icon.css"
+import categoryNavCss from "./../components/nav.css"
+import homeCss from "./styles/home.css"
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: homeCss },
+  { rel: "stylesheet", href: buttonCss },
+  { rel: "stylesheet", href: iconCss },
+  { rel: "stylesheet", href: headerCss },
+  { rel: "stylesheet", href: categoryNavCss },
+]
 
 export const loader = async ({
   request,
-}: LoaderArgs): Promise<{ address: Address | null; user: User | null }> => {
+}: LoaderArgs): Promise<{
+  addresses: Address[]
+  storesKind: StoreKind[]
+}> => {
   try {
     const phoneNumber = await requirePhoneNumber(request)
 
-    const user = await getUserByPhone({ phoneNumber })
+    const addresses = await getUserAddresses({ phoneNumber })
 
-    const address = await getNearestAddress({ phoneNumber })
+    const storesKind = await getStoresKind()
 
-    return { address, user }
+    return { addresses, storesKind }
   } catch (error) {
     throw error
   }
 }
 
 export default function Home() {
-  // const navigate = useNavigate()
-  const { address, user } = useLoaderData<typeof loader>()
+  const { addresses, storesKind } = useLoaderData<typeof loader>()
+  const [addressState, setAddressState] = useState<any>()
+
+  useEffect(() => {
+    const choosedAddressId = Number(localStorage.getItem("addressId"))
+
+    if (!choosedAddressId) return
+
+    const choosedAddress = addresses.find(
+      address => address.id == choosedAddressId,
+    )
+
+    if (!choosedAddress) return
+
+    if (!addressState) setAddressState(choosedAddress)
+
+    if (choosedAddressId !== addressState?.id) {
+      setAddressState(choosedAddress)
+    }
+  })
 
   return (
     <>
-      <form action="/logout" method="post">
-        <button type="submit">Logout</button>
-      </form>
-      <p>homeeeeee</p>
-      <Outlet></Outlet>
+      <div className="_home_container">
+        <Header
+          dir="rtl"
+          address={addressState?.address ?? "آدرس را آنتخاب کنید"}
+        ></Header>
+        <CategoryNav
+          type="Categories"
+          items={storesKind.map(kind => {
+            return {
+              name: kind.name,
+              avatarUrl: kind.avatarUrl,
+              href: `/home/stores/kind/${kind.name}`,
+            }
+          })}
+        ></CategoryNav>
+      </div>
+      <Outlet context={[addresses, setAddressState]}></Outlet>
     </>
   )
 }
