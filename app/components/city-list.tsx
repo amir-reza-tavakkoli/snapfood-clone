a2e205eb887b931a62cf3bc5123ecd05b843912c
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2023-06-15 14:16:15 +0330

    fix issues

--- app/components/city-list.tsx
+++ app/components/city-list.tsx
@@ -2,16 +2,17 @@ import "./city-list.css"
 
 type CityListProps = {
   title: string
-  type: string
+  type?: string
   items: {
     name: string
     href?: string
   }[]
+  dir? : "lrt" | "rtl"
 }
 
-export const CityList = ({ title, type, items }: CityListProps) => {
+export const CityList = ({ title, type = "City", items, dir }: CityListProps) => {
   return (
-    <article className="city-list">
+    <article className="city-list" dir={dir}>
       <p>{title}</p>
       <ul aria-label={type ?? undefined}>
         {items.map((item, index) => (
