e9daf781e708d7a8044f6892cfc5e294715cb71d
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2023-06-18 14:57:56 +0330

    take out constants

---
+++ app/constants.ts
@@ -0,0 +1,5 @@
+export const DEFAULT_CURRENCY = "تومان"
+export const DEFAULT_DELIVERY_METHOD = "ارسال اکسپرس"
+export const DEFAULT_CITY = "تهران"
+export const DEFAULT_MIN_ADDRESS_LENGTH = 10
+export const DEAFULT_DIRECTION = "rtl"
