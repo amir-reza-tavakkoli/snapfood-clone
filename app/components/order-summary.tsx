af93826b5d8858d78a6068e1e1683a19e8406fa3
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2023-06-23 14:13:59 +0330

    add orders summary links and fix bugs

---
+++ app/components/order-summary.tsx
@@ -0,0 +1,89 @@
+import { Link } from "@remix-run/react"
+
+import type { Order, Store } from "@prisma/client"
+
+import { FullOrderItem } from "~/utils/order.query.server"
+
+import { Button } from "./button"
+
+import { DEFAULT_IMG_PLACEHOLDER } from "~/constants"
+
+type OrderProps = {
+  orders:
+    | {
+        items: FullOrderItem[]
+        order: Order
+        store: Store
+      }[]
+    | undefined
+  dir?: "ltr" | "rtl"
+}
+
+export const Orders = ({ orders, dir }: OrderProps) => {
+  console.log(orders)
+
+  return (
+    <ol className="order-summary" dir={dir}>
+      <p className="nonvisual">Orders</p>
+
+      {orders
+        ? orders.map((order, index) => (
+            <li
+              data-last={orders.length - 1 == index ? true : undefined}
+              aria-label="order"
+              key={index}
+            >
+              <div>
+                <Link to={`/stores/${order.store.id}`}>
+                  <img
+                    src={order.store.avatarUrl ?? DEFAULT_IMG_PLACEHOLDER}
+                    alt=""
+                    role="presentation"
+                  />
+
+                  <p className="nonvisual">{order.store.name}</p>
+                </Link>
+                <div className="_identity">
+                  <p aria-label="Name">{order.store.name}</p>
+
+                  <time className="_datetime" aria-label="Billed At">
+                    {new Date(
+                      order.order.billDate ?? order.order.createdAt,
+                    ).toLocaleString("fa-IR")}
+
+                    <span className="_space">&nbsp;</span>
+                  </time>
+                </div>
+              </div>
+              <div className="_comment" aria-label="Comment">
+                <p>
+                  <span> نظرتان را درباره این سفارش به اشتراک بگذارید</span>
+
+                  <Link to={`/comment/${order.order.id}`}>ثبت نظر</Link>
+                </p>
+              </div>
+              <span className="_buttons">
+                <Link to={`/order/${order.order.id}`}>
+                  <Button
+                    variant="faded"
+                    icon={{ name: "info", color: "text" }}
+                    type="button"
+                  >
+                    مشاهده فاکتور
+                  </Button>
+                </Link>
+
+                <Button
+                  variant="faded"
+                  icon={{ name: "info", color: "text" }}
+                  type="button"
+                >
+                  سفارش مجدد
+                </Button>
+              </span>
+            </li>
+          ))
+        : null}
+    </ol>
+  )
+}
