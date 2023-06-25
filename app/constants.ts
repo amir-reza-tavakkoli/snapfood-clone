7d8c3cea5e81465214d1faac47d86b7914014d98
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2023-06-25 22:33:11 +0330

    extract new constants

--- app/constants.ts
+++ app/constants.ts
@@ -5,3 +5,7 @@ export const DEFAULT_MIN_ADDRESS_LENGTH = 10
 export const DEAFULT_DIRECTION = "rtl"
 export const DEFAULT_IMG_PLACEHOLDER =
   "https://snappfood.ir/static/images/placeholder.png"
+export const ALLOWED_PHONE_PREFIX = "09"
+export const VERIFICATION_CODE_FIGURES = 4
+export const VERIFICATION_CODE_EXPIRY_TIME = 5 //mins
+export const ALLOWED_URLS = ["/", "/stores", "/orders", "/login", "/addresses"]
