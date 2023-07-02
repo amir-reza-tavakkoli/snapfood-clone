ef488bb402c6e2e46479999b438d95552fa21614
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2023-07-02 15:48:38 +0330

    extract new hook from home page

---
+++ app/hooks/forceAddress.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react"
+
+import { useLocation, useNavigate } from "@remix-run/react"
+
+import type { Address } from "@prisma/client"
+
+import { DEFAULT_CITY } from "~/constants"
+
+export function useForceAddress({
+  addresses,
+}: {
+  addresses: Address[] | null
+}) {
+  const delay = 2000 // ms
+
+  const [addressState, setAddressState] = useState<Address | null>()
+  const [citystate, setCityState] = useState(DEFAULT_CITY)
+
+  let location = useLocation()
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const choosedCity = localStorage.getItem("city")
+    if (choosedCity && citystate !== choosedCity) setCityState(choosedCity)
+  })
+
+  useEffect(() => {
+    if (location.pathname === "/addresses") {
+      return
+    }
+
+    const choosedAddressId = Number(localStorage.getItem("addressId"))
+
+    if (!choosedAddressId || !addresses || addresses.length == 0) {
+      setTimeout(() => navigate("/addresses"), delay)
+      return
+    }
+
+    const choosedAddress = addresses.find(
+      address => address.id == choosedAddressId,
+    )
+
+    if (!choosedAddress) {
+      setTimeout(() => navigate("/addresses"), delay)
+      return
+    }
+
+    if (!addressState) setAddressState(choosedAddress)
+
+    if (choosedAddressId !== addressState?.id) {
+      setAddressState(choosedAddress)
+    }
+  })
+
+  return { addressState, setAddressState, citystate, setCityState }
+}
