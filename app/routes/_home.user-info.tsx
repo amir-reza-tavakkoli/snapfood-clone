e952f9bf1efa456000dcea2cc9e96cc0677ac3be
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2023-07-02 14:05:51 +0330

    add better error handling and use actionArgs

--- app/routes/_home.user-info.tsx
+++ app/routes/_home.user-info.tsx
@@ -1,32 +1,37 @@
 import { useState } from "react"
 
-import {
-  Link,
-  useActionData,
-  useLoaderData,
-  useRouteError,
-  Form,
-} from "@remix-run/react"
+import { useActionData, useLoaderData, Form } from "@remix-run/react"
 
-import type { LinksFunction, LoaderArgs } from "@remix-run/server-runtime"
+import type {
+  ActionArgs,
+  LinksFunction,
+  LoaderArgs,
+} from "@remix-run/server-runtime"
 
 import { requirePhoneNumber } from "~/utils/session.server"
 
-import { createOrUpdateUser, getUserByPhone } from "~/queries.server/user.query.server"
+import {
+  createOrUpdateUser,
+  getUserByEmail,
+  getUserByPhone,
+} from "~/queries.server/user.query.server"
 
 import type { User } from "@prisma/client"
 
 import { Button } from "~/components/button"
+import { GlobalErrorBoundary } from "~/components/error-boundary"
 
 import { DEAFULT_DIRECTION } from "~/constants"
 
-import userPageCss from "./styles/user-page.css"
+import { checkPhoneNumber, validateUser } from "~/utils/validate.server"
 
-export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: userPageCss },
-]
+import pageCss from "./styles/user-page.css"
 
-export const action = async ({ request }: any) => {
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: pageCss }]
+
+export const action = async ({
+  request,
+}: ActionArgs): Promise<{ successful?: boolean; unsuccessful?: boolean }> => {
   try {
     const form = await request.formData()
 
@@ -37,14 +42,34 @@ export const action = async ({ request }: any) => {
     const birthday = form.get("birthday")
     const email = form.get("email")
 
-    if (!phoneNumber) {
+    if (
+      !phoneNumber ||
+      typeof phoneNumber !== "string" ||
+      checkPhoneNumber(phoneNumber)
+    ) {
       throw new Error("شماره تلفن اشتباه است")
+    }
+
+    if (
+      typeof firstName !== "string" ||
+      typeof lastName !== "string" ||
+      typeof birthday !== "string" ||
+      typeof email !== "string" ||
+      gender === null
+    ) {
+      throw new Response("فرمت ورودی اشتباه است.", { status: 400 })
     }
 
     const oldUser = await getUserByPhone({ phoneNumber })
 
-    if (!oldUser) {
-      throw new Error("کاربر وجود ندارد")
+    validateUser({ user: oldUser })
+
+    if (email) {
+      const userByEmail = await getUserByEmail({ email })
+
+      if (userByEmail && userByEmail.phoneNumber !== phoneNumber) {
+        throw new Response("این ایمیل قبلا ثبت شده است.", { status: 400 })
+      }
     }
 
     const user = await createOrUpdateUser({
@@ -67,14 +92,12 @@ export const action = async ({ request }: any) => {
 }
 
 export const loader = async ({ request }: LoaderArgs): Promise<User> => {
-  const phoneNumber = await requirePhoneNumber(request)
-
   try {
-    const user = await getUserByPhone({ phoneNumber })
+    const phoneNumber = await requirePhoneNumber(request)
 
-    if (!user) {
-      throw new Error("چنین کاربری وجود ندارد")
-    }
+    let user = await getUserByPhone({ phoneNumber })
+
+    user = validateUser({ user })
 
     return user
   } catch (error) {
@@ -84,6 +107,7 @@ export const loader = async ({ request }: LoaderArgs): Promise<User> => {
 
 export default function UserInfo() {
   const user = useLoaderData<typeof loader>()
+
   const actionData = useActionData()
 
   const [firstName, setFirstName] = useState(user.firstName)
@@ -94,8 +118,9 @@ export default function UserInfo() {
 
   return (
     <main className="_user-page" dir={DEAFULT_DIRECTION}>
+      <h1>ویرایش حساب کاربری</h1>
+
       <Form method="post">
-        <p>ویرایش حساب کاربری</p>
         <div>
           <label htmlFor="phoneNumber">شماره موبایل</label>
 
@@ -119,8 +144,6 @@ export default function UserInfo() {
             id="firstName"
             value={firstName ?? undefined}
             onChange={e => {
-              e.preventDefault()
-
               if (e.target.value) {
                 setFirstName(e.target.value)
               }
@@ -139,8 +162,6 @@ export default function UserInfo() {
             id="lastName"
             value={lastName ?? undefined}
             onChange={e => {
-              e.preventDefault()
-
               if (e.target.value) {
                 setLastName(e.target.value)
               }
@@ -183,8 +204,6 @@ export default function UserInfo() {
             id="email"
             value={email ?? undefined}
             onChange={e => {
-              e.preventDefault()
-
               if (e.target.value) {
                 setEmail(e.target.value)
               }
@@ -202,8 +221,6 @@ export default function UserInfo() {
             id="birthdate"
             value={String(birthday ?? new Date(Date.now()))}
             onChange={e => {
-              e.preventDefault()
-
               if (e.target.value) {
                 setBirthday(e.target.value)
               }
@@ -231,24 +248,4 @@ export default function UserInfo() {
   )
 }
 
-export function ErrorBoundary() {
-  const error = useRouteError()
-
-  const errorMessage = error instanceof Error ? error.message : undefined
-  console.log(error)
-
-  return (
-    <div
-      aria-label="error"
-      role="alert"
-      aria-live="assertive"
-      className="boundary-error"
-    >
-      <h1>مشکلی پیش آمد!</h1>
-
-      {errorMessage ? <p>{errorMessage}</p> : null}
-
-      <Link to="/user-info">دوباره امتحان کنید</Link>
-    </div>
-  )
-}
+export const ErrorBoundary = GlobalErrorBoundary
