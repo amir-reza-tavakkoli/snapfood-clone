0b54073b3f2871bec049edf633669454052ccab6
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2023-07-10 19:58:39 +0330

    add new constants

--- app/constants.ts
+++ app/constants.ts
@@ -1,3 +1,5 @@
+import { Store } from "@prisma/client"
+
 export const VENDOR_NAME = "اسنپ فود"
 export const DEFAULT_CURRENCY = "تومان"
 export const DEFAULT_DELIVERY_METHOD = "ارسال اکسپرس"
@@ -8,8 +10,9 @@ export const DEFAULT_IMG_PLACEHOLDER =
   "https://snappfood.ir/static/images/placeholder.png"
 export const ALLOWED_PHONE_PREFIX = "09"
 export const VERIFICATION_CODE_FIGURES = 4
-export const VERIFICATION_CODE_EXPIRY_TIME = 5 //mins
+export const VERIFICATION_CODE_EXPIRY_MINS = 5 // mins
 export const ALLOWED_URLS = ["/", "/stores", "/orders", "/login", "/addresses"]
+export const INDEX_URL = "/"
 export const DEFAULT_COORDINATIONS = {
   xAxis: 35.7152,
   yAxis: 51.4043,
@@ -19,5 +22,10 @@ export const DEFAULT_DELIVERY_PRICE = 0
 export const DEFAULT_READY_TIME = 15
 export const DEFAULT_SHIPMENT_TIME = 15
 export const DEFAULT_USER_NAME = "کاربر"
-export const DEFAULT_DELIVERY_RADIUS = 10
+export const DEFAULT_DELIVERY_RADIUS = 10 // in map
 export type RESPONDED_BY = "مدیر رستوران" | typeof VENDOR_NAME
+
+export type AllowedStoresFeatures = "kind" | "discount" | "freeShipment" | "all"
+export type StoreWithTags = Store & {
+  tags?: string[]
+}
