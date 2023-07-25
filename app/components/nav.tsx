0fa67fd2078eceb7bae0ce3fa75af483e7bcea75
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2023-07-25 21:45:25 +0330

    finalize nav component

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,26 +1,33 @@
+import { DEFAULT_IMG_PLACEHOLDER } from "~/constants"
+
 export type item = {
   name: string
   avatarUrl?: string
   href: string
 }
 
-type VendorNavProps = {
+type NavProps = {
   type?: string
   items: item[]
   dir?: "lrt" | "rtl"
 }
 
-export const CategoryNav = ({ items, type, dir }: VendorNavProps) => {
+export const CategoryNav = ({ items, type, dir }: NavProps) => {
   return (
     <nav className="nav" dir={dir}>
+      <h1 className="nonvisual">Kinds</h1>
+
       <ul aria-label={type}>
         {items.map((item, index) => (
           <li key={index}>
             <a href={item.href}>
               <span>
-                {item.avatarUrl ? (
-                  <img role="presentation" src={item.avatarUrl} alt="" />
-                ) : null}
+                <img
+                  role="presentation"
+                  src={item.avatarUrl ?? DEFAULT_IMG_PLACEHOLDER}
+                  alt=""
+                />
+
                 <span className="_name">{item.name}</span>
               </span>
             </a>
