4e38cca45f76fd4eea2732b53cd1dd39d1ad13db
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2023-07-25 19:25:31 +0330

    finalize header component

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,13 +2,19 @@ import { Link } from "@remix-run/react"
 
 import { Icon } from "./icon"
 import { Button } from "./button"
-import { VENDOR_NAME } from "~/constants"
+
+import { routes } from "~/routes"
+
+import type { Address } from "@prisma/client"
+
+import { getFullAddress } from "~/utils/utils"
+
+import { VENDOR_NAME, VENDOR_NAME_ENG } from "~/constants"
 
 type HeaderProps = {
-  address: string
+  address?: Address | null
   about?: string
   dir?: "rtl" | "lrt"
-  x?: React.Dispatch<React.SetStateAction<{ z: number }>>
   toggleMenu?: React.Dispatch<React.SetStateAction<boolean>>
 }
 
@@ -17,34 +23,29 @@ export const Header = ({
   address,
   dir,
   toggleMenu,
-  x,
 }: HeaderProps) => {
   return (
     <header className="header" dir={dir}>
-      <a href="/" aria-label="Home">
-        <Icon name="snappfood" role="presentation" />
-        <span className="nonvisual">Snappfood</span>
+      <h1 className="nonvisual">{VENDOR_NAME}</h1>
+
+      <a href={routes.index} aria-label="Home" className="_vendor">
+        <Icon name={VENDOR_NAME_ENG} role="presentation" />
       </a>
+
       <div className="_identity">
         <Icon name="navigation" role="presentation"></Icon>
-        <Link
-          aria-label="Address"
-          to={"/addresses"}
-          onClick={() => (x ? x({ z: 7 }) : undefined)}
-        >
-          {address}
+
+        <Link aria-label="Address" to={routes.addresses}>
+          {address ? getFullAddress(address) : "آدرس را انتخاب کنید"}
         </Link>
-        <Button
-          variant="primary"
-          icon={{ name: "flash", color: "accent" }}
-          type="button"
-        >
-          <span className="nonvisual">Change</span>
-        </Button>
+
+        <Icon name="flash" color="accent" className="_arrow"></Icon>
       </div>
+
       <form name="search" className="_search_stores">
         <label htmlFor="__search" className="_search">
           <span className="nonvisual">Search {about}</span>
+
           <input
             autoComplete="on"
             type="search"
@@ -53,6 +54,7 @@ export const Header = ({
             placeholder="جست و جو در رستوران ها"
           />
         </label>
+
         <Button
           type="submit"
           className="_submit"
@@ -60,15 +62,19 @@ export const Header = ({
         >
           <span className="nonvisual">Submit</span>
         </Button>
+
         <Button
           className="_backup-button"
           type="button"
           icon={{ name: "search", color: "faded" }}
+          role="presentation"
         >
           <span className="nonvisual">Search</span>
         </Button>
+
         <div role="presentation"></div>
       </form>
+
       <span className="_buttons">
         <Button
           variant="primary"
@@ -79,14 +85,11 @@ export const Header = ({
         >
           <span className="nonvisual">User</span>
         </Button>
-        <Button
-          variant="primary"
-          // icon={{ name: "order", color: "text" }}
-          type="button"
-          dir={dir}
-        >
+
+        <Button variant="primary" type="button" dir={dir} className="_orders">
           <Icon name="order"></Icon>
-          <Link to={"/orders"}> سفارش ها</Link>
+
+          <Link to={routes.orders}> سفارش ها</Link>
         </Button>
       </span>
     </header>
