9c1a0c1533dfe7bff42810342df9c17e98554268
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2023-07-27 15:49:08 +0330

    add new serach route

--- app/routes/search.tsx
+++ app/routes/search.tsx
@@ -1,63 +1,37 @@
-import { Form, useFetcher, useLoaderData, useSearchParams, useSubmit } from "@remix-run/react"
-import { ActionArgs, LoaderArgs } from "@remix-run/server-runtime"
-import { useState } from "react"
+import { Item, Store } from "@prisma/client"
+
+import { LoaderArgs } from "@remix-run/server-runtime"
+
 import { searchItem, searchStore } from "~/queries.server/search.server"
 
+type LoaderType = {
+  stores: Store[]
+  itemsAndStores: {
+    item: Item
+    stores: (Store | null)[]
+  }[]
+} | null
 
-export const loader = async ({ request }: LoaderArgs) => {
+export const loader = async ({ request }: LoaderArgs): Promise<LoaderType> => {
   const search = new URL(request.url).searchParams.get("search")
-console.log("in");
+  const count = new URL(request.url).searchParams.get("count")
+  console.log(search)
 
   if (!search || typeof search !== "string" || search === "") {
     return null
   }
-  console.log(await searchStore({param : "d"}))
 
-  const stores = await searchStore({ param: search })
+  const stores = await searchStore({
+    param: search,
+    // takeThisMuch: Number(count) ?? undefined,
+  })
 
-  const itemsAndStores = await searchItem({ param: search })
+  const itemsAndStores = await searchItem({
+    param: search,
+    // takeThisMuch: Number(count) ?? undefined,
+  })
 
   return { stores, itemsAndStores }
-
 }
 
-
-export default function SomeComponent() {
-  const fetcher = useFetcher()
-  let [searchParams, setSearchParams] = useSearchParams()
-  const x = useLoaderData()
-
-  console.log(x)
-
-  const [state, setstate] = useState("")
-
-
-
-  const [marker, setMarker] = useState<any>(null)
-
-  function handleChange(event: any) {
-    console.log(event.currentTarget)
-    submit(event.currentTarget, { replace: true })
-  }
-
-  const submit = useSubmit()
-  return (
-    <Form
-      replace={false}
-      reloadDocument={false}
-      // ref={setMarker}
-      // onChange={e => handleChange(e)}
-    >
-      <input
-        id="ddd"
-        type="text"
-        name="search"
-        onChange={e => {
-          setSearchParams({search: e.target.value})
-          setstate(e.target.value)
-          // handleChange(e)
-        }}
-      />
-    </Form>
-  )
-}
+export { type LoaderType as SearchType }
