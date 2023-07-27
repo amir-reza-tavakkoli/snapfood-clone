f99a41747f563cffe8977c6cef747b21e2186bda
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2023-07-27 15:50:42 +0330

    add search bar for mobile phones

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,4 +1,4 @@
-import { Link } from "@remix-run/react"
+import { Form, Link } from "@remix-run/react"
 
 import { Icon } from "./icon"
 import { Button } from "./button"
@@ -9,7 +9,13 @@ import type { Address } from "@prisma/client"
 
 import { getFullAddress } from "~/utils/utils"
 
-import { VENDOR_NAME, VENDOR_NAME_ENG } from "~/constants"
+import {
+  DEFAULT_IMG_PLACEHOLDER,
+  VENDOR_NAME,
+  VENDOR_NAME_ENG,
+} from "~/constants"
+import { useEffect, useState } from "react"
+import { SearchType } from "~/routes/search"
 
 type HeaderProps = {
   address?: Address | null
@@ -24,6 +30,24 @@ export const Header = ({
   dir,
   toggleMenu,
 }: HeaderProps) => {
+  const [searchValue, setSearchValue] = useState("")
+
+  const [searchData, setSearchData] = useState<SearchType>()
+
+  useEffect(() => {
+    async function fetchData() {
+      if(searchValue == "")return
+      const data = await fetch(routes.search + `?search=${searchValue}`)
+
+      const jsonData = (await data.json()) as unknown as SearchType
+
+      setSearchData(jsonData)
+    }
+
+    fetchData()
+  }, [searchValue])
+  console.log(searchData)
+
   return (
     <header className="header" dir={dir}>
       <h1 className="nonvisual">{VENDOR_NAME}</h1>
@@ -42,7 +66,12 @@ export const Header = ({
         <Icon name="flash" color="accent" className="_arrow"></Icon>
       </div>
 
-      <form name="search" className="_search_stores">
+      <Form
+        name="search"
+        className="_search_stores"
+        replace={false}
+        reloadDocument={false}
+      >
         <label htmlFor="__search" className="_search">
           <span className="nonvisual">Search {about}</span>
 
@@ -52,6 +81,9 @@ export const Header = ({
             name="search"
             id="__search"
             placeholder="جست و جو در رستوران ها"
+            onChange={e => {
+              setSearchValue(e.target.value)
+            }}
           />
         </label>
 
@@ -73,7 +105,7 @@ export const Header = ({
         </Button>
 
         <div role="presentation"></div>
-      </form>
+      </Form>
 
       <span className="_buttons">
         <Button
@@ -92,6 +124,63 @@ export const Header = ({
           <Link to={routes.orders}> سفارش ها</Link>
         </Button>
       </span>
+
+      {searchData &&
+      (searchData.stores || searchData.itemsAndStores) &&
+      (searchData.stores.length > 0 || searchData?.itemsAndStores.length > 0) &&
+      searchValue &&
+      searchValue !== "" ? (
+        <ul className="search-bar">
+          {searchData.stores && searchData.stores.length > 0 ? <p>فروشگاه ها</p> : null}
+          {searchData?.stores?.map((store, index) => (
+            <li key={index} aria-label="Store">
+              <Link to={routes.store(store.id)}>
+                {" "}
+                {store.name}
+                <img
+                  src={store.avatarUrl ?? DEFAULT_IMG_PLACEHOLDER}
+                  alt=""
+                  role="presentation"
+                />
+              </Link>
+            </li>
+          ))}
+
+          {searchData.itemsAndStores && searchData.itemsAndStores.length > 0 ? (
+            <p>آیتم ها</p>
+          ) : null}
+
+          {searchData?.itemsAndStores?.map((combo, index) => (
+            <li key={index} aria-label="Store">
+              {combo.item.name}
+              <img
+                src={combo.item.avatarUrl ?? DEFAULT_IMG_PLACEHOLDER}
+                alt=""
+                role="presentation"
+              />
+              <ul>
+                {combo.stores.map((store, index) =>
+                  store ? (
+                    <li key={index}>
+                      <Link to={routes.store(store.id)}>
+                        {store.name}
+                        <img
+                          src={store.avatarUrl ?? DEFAULT_IMG_PLACEHOLDER}
+                          alt=""
+                        />
+                      </Link>
+                    </li>
+                  ) : null,
+                )}
+              </ul>
+            </li>
+          ))}
+        </ul>
+      ) : searchValue && searchValue !== "" ? (
+        <ul className="search-bar">
+          <p>آیتمی یافت نشد</p>
+        </ul>
+      ) : null}
     </header>
   )
 }
