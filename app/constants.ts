8629981a5b24ed9982394e87111abac28a83adea
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2023-07-29 18:40:33 +0330

    and new length constants

--- app/constants.ts
+++ app/constants.ts
@@ -19,19 +19,28 @@ export const DEFAULT_COORDINATIONS = {
   yAxis: 51.4043,
 } // tehran
 export const DEFAULT_MAP_ZOOM = 13
-export const DEFAULT_DELIVERY_PRICE = 0
+export const DEFAULT_SHIPMENT_PRICE = 0
 export const DEFAULT_READY_TIME = 15
 export const DEFAULT_SHIPMENT_TIME = 15
 export const DEFAULT_USER_NAME = "کاربر"
-export const DEFAULT_DELIVERY_RADIUS = 10 // in map
+export const DEFAULT_SHIPMENT_RADIUS = 10 // in map
 export type RESPONDED_BY = "مدیر رستوران" | typeof VENDOR_NAME
-
+export const PER_UNIT_ADDED_TIME = 2
 export type AllowedStoresFeatures = "kind" | "discount" | "freeShipment" | "all"
 export type StoreWithTags = Store & {
   tags?: string[]
 }
-
+export const MAX_COMMENT_SIZE = 160
 export const SCORE_ROUNDING = 2
 export const READY_TIME_OFFSET = 15
 
 export const COOKIE_ADDRESS = "addressId"
+export const COOKIE_City = "city"
+
+export const SVG_ICON_PREFIX = "_svg_"
+
+export const MIN_NAME_LENGTH = 3
+export const MAX_NAME_LENGTH = 30
+
+export const MIN_EMAIL_LENGTH = 3
+export const MAX_EMAIL_LENGTH = 30
