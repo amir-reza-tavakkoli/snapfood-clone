32e93c2bb58e8819db35b153011c46ef2f3898f3
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2023-07-29 12:12:23 +0330

    fix intro banner issues

--- app/components/intro-banner.tsx
+++ app/components/intro-banner.tsx
@@ -1,39 +1,66 @@
-import { StoreKind } from "@prisma/client"
 import { Link } from "@remix-run/react"
-import { DEAFULT_DIRECTION, VENDOR_NAME_ENG } from "~/constants"
-import { routes } from "~/routes"
+
+import type { StoreKind } from "@prisma/client"
+
+import { routes } from "../routes"
+
 import { Icon } from "./icon"
 import { CategoryNav } from "./nav"
 
+import { DEAFULT_DIRECTION, VENDOR_NAME_ENG } from "../constants"
+
 type IntroBannerProps = {
-    city: string
-    storesKind : StoreKind[]
+  city?: string
+  storesKind: StoreKind[]
 }
 
-export function IntroBanner({storesKind, city = ""} : IntroBannerProps ) {
+export function IntroBanner({ storesKind, city = "" }: IntroBannerProps) {
   return (
-    <header className="intro-banner">
+    <main className="intro-banner">
       <div>
-        <Icon name={VENDOR_NAME_ENG}></Icon>
-        <Link to={routes.notImplemented}>ثبت نام فروشندگان</Link>
-        <Link to={routes.login}> ورود یا عضویت</Link>
+        <span>
+          <Link to={routes.notImplemented}>
+            ثبت نام فروشندگان
+            <Icon name="store" color="text" className="_store-owner"></Icon>
+          </Link>
+
+          <Link to={routes.login}> ورود یا عضویت</Link>
+        </span>
+
+        <Link to={routes.index} className="_logo">
+          <Icon name={VENDOR_NAME_ENG}></Icon>
+        </Link>
       </div>
-      <div>
-        <h1>سفارش آنلاین</h1>
-              <p>سفارش آنلاین غذا ، میوه، نان، شیرینی و ...</p>
-              <img src="https://snappfood.ir/static/images/hero-image.png" alt="" />
+
+      <div aria-label="About">
+        <h1>سفارش آنلاین </h1>
+
+        <p aria-label="Services">
+          سفارش آنلاین غذا ، میوه، نان، شیرینی و
+          <br />
+          ...
+        </p>
       </div>
-      <CategoryNav
-        dir={DEAFULT_DIRECTION}
-        type="Categories"
-        items={storesKind.map(kind => {
-          return {
-            name: kind.name,
-            avatarUrl: kind.avatarUrl,
-            href: `/stores/${city}/kind/${kind.name}`,
-          }
-        })}
-      ></CategoryNav>
-    </header>
+
+      <img
+        src="https://snappfood.ir/static/images/hero-image.png"
+        alt=""
+        role="prsentation"
+      />
+
+      {storesKind ? (
+        <CategoryNav
+          dir={DEAFULT_DIRECTION}
+          type="Categories"
+          items={storesKind.map(kind => {
+            return {
+              name: kind.name,
+              avatarUrl: kind.avatarUrl,
+              href: routes.storesKind(city, kind.name),
+            }
+          })}
+        ></CategoryNav>
+      ) : null}
+    </main>
   )
 }
