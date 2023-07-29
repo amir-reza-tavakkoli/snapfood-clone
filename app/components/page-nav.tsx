140bc02349777bab899b49b21a01a9656160e23e
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2023-07-29 13:54:52 +0330

    fix page nav issues

--- app/components/page-nav.tsx
+++ app/components/page-nav.tsx
@@ -1,25 +1,29 @@
 import { Link, useLocation } from "@remix-run/react"
 
-import { routes } from "~/routes"
+import { routes } from "../routes"
 
 import { Icon } from "./icon"
 
 export function PageNav() {
   const location = useLocation()
+
   return (
     <nav className="page-nav">
       <ul>
+        <p className="nonvisual">Links</p>
+
         <li>
           <Link
             to={routes.index}
             className={
               location.pathname === routes.index ||
-              location.pathname === routes.stores
+              location.pathname === routes.stores || location.pathname.match(routes.stores)
                 ? "_choosed"
                 : undefined
             }
           >
             <span>خانه</span>
+
             <Icon name="home"></Icon>
           </Link>
         </li>
@@ -32,6 +36,7 @@ export function PageNav() {
             }
           >
             <span>سفارش ها</span>
+
             <Icon name="order"></Icon>
           </Link>
         </li>
@@ -44,6 +49,7 @@ export function PageNav() {
             }
           >
             <span>سبد خرید</span>
+
             <Icon name="cart"></Icon>
           </Link>
         </li>
