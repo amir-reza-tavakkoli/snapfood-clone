2c8cf7514038354e1cec2de06c4298e09c97224b
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2023-08-01 17:58:46 +0330

    update document when error happended

--- app/components/error-boundary.tsx
+++ app/components/error-boundary.tsx
@@ -1,16 +1,27 @@
-import { Link, useNavigate, useRouteError } from "@remix-run/react"
-
-import { routes } from "../routes"
+import {
+  isRouteErrorResponse,
+  Link,
+  useNavigate,
+  useRouteError,
+} from "@remix-run/react"
 
 import { Icon } from "./icon"
+
+import { routes } from "../routes"
+import { useEffect } from "react"
 
 export function GlobalErrorBoundary() {
   const error = useRouteError()
 
+  const errorMessage = error instanceof Error ? error.message : undefined
+
+  const errorStack = error instanceof Error ? error.stack : undefined
+
   const navigate = useNavigate()
 
-  const errorMessage = error instanceof Error ? error.message : undefined
-  const errorStack = error instanceof Error ? error.stack : undefined
+  useEffect(() => {
+    document ? (document.title = "Error Snappfood Clone" + errorMessage) : null
+  }, [])
 
   return (
     <output
@@ -21,8 +32,16 @@ export function GlobalErrorBoundary() {
     >
       <h1>مشکلی پیش آمد!</h1>
 
+      {isRouteErrorResponse(error) ? <p>{error?.data}</p> : null}
+
+      {isRouteErrorResponse(error) ? (
+        <p>{error.status.toLocaleString("fa")} خطای </p>
+      ) : null}
+
       {errorMessage ? <p>{errorMessage}</p> : null}
+
       <br />
+
       {errorStack ? <p>{errorStack}</p> : null}
 
       <Link to={routes.index}>
