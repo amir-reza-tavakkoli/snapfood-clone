5b989cd0dd7f4e6c19fa7e14b467b7cdc0e8f233
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2023-08-10 16:48:59 +0330

    fix store info page issues

--- app/routes/_home.store-info.$storeId.tsx
+++ app/routes/_home.store-info.$storeId.tsx
@@ -1,4 +1,6 @@
-import { useLoaderData, V2_MetaFunction } from "@remix-run/react"
+import { useState } from "react"
+
+import { Link, useLoaderData, V2_MetaFunction } from "@remix-run/react"
 
 import {
   json,
@@ -8,6 +10,8 @@ import {
 } from "@remix-run/server-runtime"
 
 import type { Address, Store, storeSchedule } from "@prisma/client"
+
+import type { Map } from "leaflet"
 
 import {
   getStore,
@@ -23,10 +27,16 @@ import { requireUser, validateNumberParam } from "../utils/validate.server"
 
 import { Icon } from "../components/icon"
 import { GlobalErrorBoundary } from "../components/error-boundary"
+import { ClientOnly } from "~/client.map"
+import { MapComponent } from "~/components/map.client"
 import { CommentComp } from "../components/comment"
+
+import { routes } from "~/routes"
 
 import {
   CLIENT_CACHE_DURATION,
+  DEFAULT_COORDINATIONS,
+  DEFAULT_CURRENCY,
   DEFAULT_IMG_PLACEHOLDER,
   StoreComment,
 } from "../constants"
@@ -37,6 +47,10 @@ import pageCss from "./styles/store-info-page.css"
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: pageCss },
   { rel: "stylesheet", href: commentCss },
+  {
+    rel: "stylesheet",
+    href: "https://unpkg.com/leaflet@1.8.0/dist/leaflet.css",
+  },
 ]
 
 export const meta: V2_MetaFunction<LoaderType> = ({ data }) => {
@@ -109,30 +123,53 @@ export default function StoreInfoPage() {
 
   const now = new Date(Date.now())
 
+  const [map, setMap] = useState<Map | null>(null)
+
   const schedule = getStoreCurrentSchedule(schedules)
 
   return (
     <main className="store-info">
-      <address aria-label="Info">
-        <img
-          width={40}
-          height={40}
-          src={store.avatarUrl ?? DEFAULT_IMG_PLACEHOLDER}
-          alt=""
-          role="presentation"
-          loading="lazy"
-        />
+      <Link to={routes.store(store.id)}>
+        <address aria-label="Info">
+          <img
+            width={40}
+            height={40}
+            src={store.avatarUrl ?? DEFAULT_IMG_PLACEHOLDER}
+            alt=""
+            role="presentation"
+            loading="lazy"
+          />
 
-        <div>
-          <h1>{store.name}</h1>
+          <div>
+            <h1>{store.name}</h1>
 
-          <small>{categories.join(", ")}</small>
+            <small>{categories.join(", ")}</small>
 
-          <Icon name="navigation" color="faded"></Icon>
+            <p>
+              <Icon name="navigation" color="faded"></Icon>
+              {getFullAddress(address)}
+            </p>
+          </div>
 
-          <p>{getFullAddress(address)}</p>
-        </div>
-      </address>
+          <div className="_map-container">
+            <ClientOnly
+              fallback={<div style={{ height: 100, background: "#d1d1d1" }} />}
+            >
+              {() => (
+                <MapComponent
+                  map={map}
+                  setMap={setMap}
+                  height={"200px"}
+                  initPosition={[
+                    address.xAxis ?? DEFAULT_COORDINATIONS.xAxis,
+                    address.yAxis ?? DEFAULT_COORDINATIONS.yAxis,
+                  ]}
+                />
+              )}
+            </ClientOnly>
+          </div>
+        </address>
+      </Link>
 
       <dl>
         <span>
@@ -161,8 +198,8 @@ export default function StoreInfoPage() {
           <Icon name="pay"></Icon>
 
           <dd>
-            {store.takesOfflineOrder ? "آفلاین" : null}
-            ,آنلاین
+            {store.takesOfflineOrder ? "  آفلاین" + ", " : null}
+            آنلاین
           </dd>
         </span>
 
@@ -171,44 +208,50 @@ export default function StoreInfoPage() {
 
           <Icon name="cart"></Icon>
 
-          <dd>{store.minOrderPrice.toLocaleString("fa")}</dd>
+          <dd>
+            {store.minOrderPrice.toLocaleString("fa") + " " + DEFAULT_CURRENCY}
+          </dd>
         </span>
       </dl>
 
-      <div>
-        <p className="nonvisual">امتیازات فروشگاه</p>
-        <span>
-          <span className="_store-score">
-            {store.score.toLocaleString("fa")}
+      {store.score && store.score > 0 ? (
+        <div>
+          <p className="nonvisual">امتیازات فروشگاه</p>
+          <span>
+            <span className="_store-score">
+              {store.score.toLocaleString("fa")}
 
-            <Icon name="star"></Icon>
+              <Icon name="star"></Icon>
+            </span>
           </span>
-        </span>
-
-        <span>
-          <span> امتیاز </span>
 
           <span>
-            از مجموع
-            <span> {store.scoreCount.toLocaleString("fa")} </span>
-            نظر
-          </span>
-        </span>
-      </div>
+            <span> امتیاز </span>
 
-      <p>نظرات کاربران</p>
+            <span>
+              از مجموع
+              <span> {store.scoreCount.toLocaleString("fa")} </span>
+              نظر
+            </span>
+          </span>
+        </div>
+      ) : null}
+
+      {comments && comments.length > 0 ? <p>نظرات کاربران</p> : null}
 
       <ul>
-        {comments.map((comment, index) =>
-          comment &&
-          comment.comment &&
-          comment.comment.description &&
-          comment.comment.isVisible &&
-          comment.comment.isVerified &&
-          comment.comment.score ? (
-            <CommentComp comment={comment} index={index}></CommentComp>
-          ) : null,
-        )}
+        {comments &&
+          comments.length > 0 &&
+          comments.map((comment, index) =>
+            comment &&
+            comment.comment &&
+            comment.comment.description &&
+            comment.comment.isVisible &&
+            comment.comment.isVerified &&
+            comment.comment.score ? (
+              <CommentComp comment={comment} index={index}></CommentComp>
+            ) : null,
+          )}
       </ul>
     </main>
   )
